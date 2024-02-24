369bb6f5d1daadd474f63b37d5a7b7ee021f1be3
Denis Taranov <taranov.denis.95@gmail.com>
2024-02-24 13:46:39 +0300

    added search in admin

--- fritterustv_bot/queries/admin.py
+++ fritterustv_bot/queries/admin.py
@@ -6,8 +6,10 @@ from . import models
 @admin.register(models.Query)
 class QueryAdmin(admin.ModelAdmin):
     list_display = ('title', 'description')
+    search_fields = ('title',)
 
 
 @admin.register(models.QueryAnswer)
 class QueryAnswerAdmin(admin.ModelAdmin):
     list_display = ('pattern', 'query')
+    search_fields = ('query__title', 'pattern', 'clean_text')
